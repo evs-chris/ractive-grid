7f9d3e956c7a61085cc5c7bb4e93f61d34028880
Chris Reeves <chris@ev-soft.net>
2014-07-15 11:29:23 -0400

    Adds div-based layout, keyboard support, tracking of current item, and filtering. Some attributes may now be passed through (class, id, style) from the template component to the actual DOM element.

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 var Ractive = require('ractive');
 
-var template = "<table class='ractive-grid'>" +
+var templateTable = "<table class='ractive-grid' on-keydown='selection' tabindex='0'|>" +
   "  <thead>" +
   "    <tr>{{#headers:i}}<th on-click='headerClicked:{{i}}' class='{{#sortDir(i) === 1}}header-sort-asc{{/}}{{#sortDir(i) === 2}}header-sort-desc{{/}}'>{{.}}</th>{{/headers}}</tr>" +
   "  </thead>" +
   "  <tbody>" +
   "    {{#rendered}}" +
   "      {{#itemsView:i}}" +
-  "        {{#editingRow(i)}}" +
-  "        <tr><td colspan='{{columns.length}}'>Editing!</td></tr>" +
-  "        {{/}}" +
-  "        {{^editingRow(i)}}" +
-  "        <tr on-dblclick='rowDoubleClicked:{{i}}'>{{>row}}</tr>" +
-  "        {{/}}" +
+  "        <tr on-click='rowClicked:{{i}}' on-dblclick='rowDoubleClicked:{{i}}' class='{{#isCurrentRow(i)}}current{{/}}'>{{>tableRow}}</tr>" +
   "      {{/items}}" +
   "    {{/rendered}}" +
   "  </tbody>" +
   "</table>";
+
+var templateDiv = "<div class='ractive-grid' on-keydown='selection' tabindex='0'|>" +
+  "  <div class='rg-header'>{{#headers:i}}<div on-click='headerClicked:{{i}}' class='{{#sortDir(i) === 1}}header-sort-asc{{/}}{{#sortDir(i) === 2}}header-sort-desc{{/}} {{columnClass(i)}}'>{{.}}</div>{{/headers}}</div>" +
+  "  <div class='rg-body'>" +
+  "    {{#rendered}}" +
+  "      {{#itemsView:i}}" +
+  "        <div class='rg-row{{#isCurrentRow(i)}} current{{/}}' on-dblclick='rowDoubleClicked:{{i}}' on-click='rowClicked:{{i}}' >{{>divRow}}</div>" +
+  "      {{/}}" +
+  "    {{/}}" +
+  "  </div>" +
+  "</div>";
 
 (function() {
   var getProp = function getProp(obj, path) {
@@ -31,11 +37,36 @@ var template = "<table class='ractive-grid'>" +
 
   var Grid;
   Grid = Ractive.extend({
-    template: template,
+    template: '{{#table}}{{>table}}{{/}}{{^table}}{{>div}}{{/}}',
+    beforeInit: function(opts) {
+      var transfer = ['class', 'style', 'id'];
+      var str = '';
+      for (var k in opts.data) if (transfer.indexOf(k) >= 0) str += ' ' + k + '="' + opts.data[k] + '"';
+      opts.partials.table = templateTable.replace(/\|/, str);
+      opts.partials.div = templateDiv.replace(/\|/, str);
+    },
     init: function() {
       var grid = this;
       grid.on('rowDoubleClicked', function(e, i) {
-        grid.fire('rowSelected', e, grid.get('itemsView.' + i), i);
+        grid.fire('rowSelected', grid.get('itemsView.' + i), i, e);
+      });
+      grid.on('rowClicked', function(e, i) {
+        grid.set('currentIndex', i);
+      });
+      grid.on('selection', function(e) {
+        var current = grid.get('currentIndex');
+        if (typeof current !== 'number') return;
+        var items = grid.get('itemsView');
+        if (e.original.keyCode === 38 && current > 0) current--;
+        else if (e.original.keyCode === 40) current++;
+        else if (e.original.keyCode === 13) {
+          grid.fire('rowSelected', grid.get('current'), current, e);
+          e.original.preventDefault();
+          return;
+        }
+        else return;
+        e.original.preventDefault();
+        grid.current(current);
       });
       grid.on('colDoubleClicked', function(e, arr) {
         grid.fire('colSelected', e, grid.get('itemsView.' + arr[0]), arr[1], arr[0]);
@@ -75,19 +106,36 @@ var template = "<table class='ractive-grid'>" +
       sortDir: function(idx) {
         return this.get('sorts')[idx];
       },
+      columnClass: function(idx) {
+        return ((this.get('_columns') || [])[idx] || {}).class;
+      },
+      isCurrentRow: function(idx) {
+        return this.get('currentIndex') === idx;
+      },
       editingRows: [],
       columns: [],
       sorts: {order: []},
       sortable: true,
-      multisortable: true
+      multisortable: true,
+      table: true
     },
     computed: {
       itemsView: function() {
         var grd = this;
         this.get('sorts');
+        var filter = this.get('_filter');
         var sorts = this.get('sorting') || [];
         var items = (this.get('items') || []).slice(0);
         var columns = this.get('_columns' + '') || [];
+        var flted, i;
+        if (typeof filter === 'function') {
+          flted = [];
+          for (i = 0; i < items.length; i++) {
+            if (filter(items[i])) flted.push(items[i]);
+          }
+          items = flted;
+        }
+        if (this.get('currentIndex' + '') >= items.length) this.set('currentIndex', items.length - 1);
         items.sort(function(a, b) {
           for (var i in sorts) {
             var s = sorts[i];
@@ -105,6 +153,12 @@ var template = "<table class='ractive-grid'>" +
           return 0;
         });
         return items;
+      },
+      current: function() {
+        var items = this.get('itemsView');
+        var idx = this.get('currentIndex');
+        if (typeof idx !== 'number') return null;
+        return (items || [])[idx];
       },
       sorting: function() {
         var res = [];
@@ -143,7 +197,10 @@ var template = "<table class='ractive-grid'>" +
       }
     },
     partials: {
-      row: '',
+      tableRow: '',
+      divRow: '',
+      table: '',
+      div: ''
     },
     columns: function(arr) {
       if (!!!arr) { // init columns from items
@@ -168,15 +225,41 @@ var template = "<table class='ractive-grid'>" +
           else return 0;
         });
         this.set('_columns', arr);
-        var str = '';
-        for (var c in arr) {
-          str += '<td on-dblclick=\'colDoubleClicked:[{{i}},' + c + ']\'>{{' + arr[c].path + '}}</td>';
+        var tstr = '', dstr = '';
+        for (var c = 0; c < arr.length; c++) {
+          tstr += '<td on-dblclick=\'colDoubleClicked:[{{i}},' + c + ']\' class=\'{{columnClass(i)}}\'>{{' + arr[c].path + '}}</td>';
+          dstr += '<div on-dblclick=\'colDoubleClicked:[{{i}},' + c + ']\' class=\'{{columnClass(i)}}\'>{{' + arr[c].path + '}}</div>';
         }
-        this.partials.row = str;
+        this.partials.tableRow = tstr;
+        this.partials.divRow = dstr;
         this.set('rendered', false);
         this.set('rendered', true);
         return arr;
       }
+    },
+    current: function(idx) {
+      if (arguments.length === 0) return this.get('current')();
+      var items = this.get('itemsView');
+      if (idx < 0 || idx >= items.length) return;
+      this.set('currentIndex', idx);
+      var tbl = this.get('table');
+
+      var n, p;
+      if (tbl) {
+        n = this.el.querySelector('tbody').children[idx];
+        p = this.el;
+      } else {
+        n = this.el.querySelector('.rg-body').children[idx];
+        if (n.style.overflow !== '') p = n;
+        else p = this.el;
+      }
+
+      if (n.offsetTop < p.scrollTop) p.scrollTop = n.offsetTop;
+      else if (n.offsetTop > p.scrollTop + p.clientHeight) p.scrollTop = (n.offsetTop - p.clientHeight / 2);
+    },
+    filter: function(fn) {
+      this.set('_filter', fn);
+      this.update('itemsView');
     }
   });
 
